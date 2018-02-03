cc6cce9637676ac89f6cc69e6973f96fa64e2243
Johnathan Lyman <jelyman@gmail.com>
2018-02-02 17:18:05 -0800

    error page edits (again)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # The homepage
   root "homepage#index"
-  resources :homepage, only: :index
+
+  # Contact Form
   resources :contacts, only: :create
   get 'contact', to: 'contacts#new', as: 'contact'
+
+  # Error Handling
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
