8cc613d73c94b05d6b55a53cfeaba97a120b2906
Johnathan Lyman <jelyman@gmail.com>
2018-02-03 14:00:35 -0800

    added some basic profiling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => 'session'
   # The homepage
   root "homepage#index"
 
   # Contact Form
-  resources :contacts, only: :create
+  resources :contacts, only: :create, :path => 'contact'
   get 'contact', to: 'contacts#new', as: 'contact'
+
+  # Profile page
+  resource :user, :path => 'profile', as: 'profile', only: [:show, :update]
 
   # Error Handling
   match "/404", :to => "errors#not_found", :via => :all
