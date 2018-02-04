47d65b73d553c7f6e27b45402bd1ed8983e84502
Johnathan Lyman <jelyman@gmail.com>
2018-02-03 19:32:28 -0800

    extended users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => 'session'
   # The homepage
   root "homepage#index"
 
@@ -7,8 +6,15 @@ Rails.application.routes.draw do
   resources :contacts, only: :create, :path => 'contact'
   get 'contact', to: 'contacts#new', as: 'contact'
 
-  # Profile page
-  resource :user, :path => 'profile', as: 'profile', only: [:show, :update]
+  # Devise
+  devise_for :users, :path => "user", skip: [:sessions]
+
+  # Overrides
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   # Error Handling
   match "/404", :to => "errors#not_found", :via => :all
